4bc48b6b69494c315a51eb1813b78815de4fcf86
Viper <momo2010sofia@gmail.com>
2024-07-03 23:21:46 +0100

    ImGui

--- src/main.cpp
+++ src/main.cpp
@@ -16,9 +16,7 @@ std::vector<Button> buttonlist = {
     {"Button 3", false}
 };
 
-auto winSize = CCDirector::get()->getWinSize();
-ImVec2 windowPos = ImVec2(300, 100); 
-ImVec2 windowSize = ImVec2(winSize.width / 2, 100);
+ImVec2 windowPos = ImVec2(1000, 100); 
 
 $on_mod(Loaded) {
     ImGuiCocos::get().setup([] {
@@ -29,13 +27,13 @@ $on_mod(Loaded) {
         ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoTitleBar;
 
         ImGui::SetNextWindowPos(windowPos, ImGuiCond_Always);
-        ImGui::SetNextWindowSize(windowSize, ImGuiCond_Always);
+        ImGui::SetWindowSize(ImVec2(500, 500));
 
         ImGui::Begin("My awesome window", nullptr, window_flags);
 
         for (size_t i = 0; i < buttonlist.size(); ++i) {
             ImVec4 buttonColor = buttonlist[i].isChecked ? ImVec4(0.06f, 0.53f, 0.98f, 1.00f) : ImVec4(0.00f, 0.00f, 0.00f, 0.40f);
-            ImVec4 buttonHoveredColor = ImVec4(1.00f, 1.00f, 1.00f, 0.36f);
+            ImVec4 buttonHoveredColor = buttonlist[i].isChecked ? ImVec4(0.06f, 0.53f, 0.98f, 1.00f) : ImVec4(1.00f, 1.00f, 1.00f, 0.36f);
             ImVec4 buttonActiveColor = buttonlist[i].isChecked ? ImVec4(0.06f, 0.53f, 0.98f, 1.00f) : ImVec4(0.00f, 0.00f, 0.00f, 0.40f);
 
             ImGui::PushStyleColor(ImGuiCol_Button, buttonColor);
